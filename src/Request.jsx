7d9277358a632312f304329163bcc6eba849f6ef
Edward <a0966219696@gmail.com>
2024-07-21 23:13:26 +0800

    fix env file

--- src/Request.jsx
+++ src/Request.jsx
@@ -1,4 +1,4 @@
-const key = "47a497dd75198ec5af83474ae532ce0a";
+const key = import.meta.env.VITE_TMDB_API_KEY;
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
